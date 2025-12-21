e9da2134ec2c55d6b6e5a67abb6e8635f8ddf341
priyankraychura <priyankraychura@gmail.com>
2025-12-21 14:11:58 +0530

    Add array and object type examples in TypeScript
    Expanded 04_myObjects.ts with more detailed object type definitions, readonly properties, and intersection types. Added 05_myArray.ts to demonstrate array types, including arrays of primitives, objects, and multidimensional arrays.

--- 04_myObjects.ts
+++ 04_myObjects.ts
@@ -6,8 +6,52 @@ const User = {
 
 function createUser({name, isPaid}: {name: string, isPaid: boolean}) {}
 
-createUser({name: "Priyank", isPaid: false})
+let newUser = {name: "Priyank", isPaid: false, email: "priyank@gmail.com"}
 
-function createCourse(): {} {
-    return {}
+createUser(newUser)
+
+function createCourse(): {name: string, price: number} {
+    return {name: "reactjs", price: 399}
 }
+
+// type
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
+}
+
+function createUser1(user: User): User {
+    return {name: "", email: "", isActive: true}
+}
+createUser1({name: "", email: "", isActive: true})
+
+// 
+type newUser = {
+    readonly _id: string
+    name: string
+    email: string
+    isActive: boolean
+    creditCardDetails?: number
+}
+
+let myUser: newUser = {
+    _id: "1234",
+    name: "Priyank",
+    email: "priyank@gmail.com",
+    isActive: false
+}
+
+type cardNumber = {
+    cardnumber: string
+}
+
+type cardDate = {
+    cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+myUser.email = "pvr@gmail.com"
---
+++ 05_myArray.ts
@@ -0,0 +1,24 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type userObject = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: userObject[] = []
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
+
+console.log(allUsers)
+
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
