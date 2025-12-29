82582845cc7d807890593331b050170a4317075f
priyankraychura <priyankraychura@gmail.com>
2025-12-29 10:05:25 +0530

    Learned Interface

---
+++ 09_myInterface.ts
@@ -0,0 +1,28 @@
+interface User {
+    readonly dbId: number,
+    email: string,
+    userId: number,
+    googleId?: string,
+    // startTrial: () => string,
+    startTrial(): string
+    getCoupon(couponname: string, value: number): number
+
+}
+
+const priyank: User = {
+    dbId: 1234,
+    email: "priyank@gmail.com",
+    userId: 12345,
+    startTrial: () => {
+        return "trial started"
+    },
+    getCoupon:(name: "priyank", off: 10) => {
+        return 10
+    }
+}
+
+priyank.email = "pvr@gmail.com"
+// priyank.dbId = 12345
+
+
+
