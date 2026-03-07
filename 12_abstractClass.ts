92e9e251e1db670ccecbe08c5508b9cf5556e3d7
priyankraychura <priyankraychura@gmail.com>
2026-03-07 11:28:36 +0530

    abstract classes learned

---
+++ 12_abstractClass.ts
@@ -0,0 +1,32 @@
+abstract class TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string
+    ) {}
+
+    abstract getSepia(): void
+
+    getReelTime(): number {
+        // some complex calculation
+        return 8
+    }
+}
+
+class Instagram extends TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ) {
+        super(cameraMode, filter)
+    }
+
+    getSepia(): void {
+        console.log("Sepia")
+    }
+}
+
+const pr = new Instagram("test", "Test", 123)
+pr.getReelTime()
+pr.getSepia()
+console.log(pr)
